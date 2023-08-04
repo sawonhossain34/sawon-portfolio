ba1872c8def1f51b25cbb2a4840e5f99866617d1
sawonhossain34 <sawonhossain3491@gmail.com>
2023-08-04 17:27:32 +0600

    project add left and right

--- src/components/ProjectsSection/Projects.jsx
+++ src/components/ProjectsSection/Projects.jsx
@@ -2,9 +2,28 @@ import './Projects.css';
 
 const Projects = () => {
     return (
-        <div>
-            <h2>Projects</h2>
-        </div>
+        <>
+            <div className='px-5 max-w-[1560px] mx-auto py-10'>
+                {/* top */}
+                <div className='flex justify-between items-center gap-5'>
+                    {/* left */}
+                    <div className='text-white font-medium text-3xl flex items-center gap-2'>
+                        <div className=''>
+                            <span className='text-[#C778DD]'>#</span>Projects
+                        </div>
+                        <div className='line w-2/3 h-px bg-[#C778DD]'></div>
+                    </div>
+                    {/* right */}
+                    <div className='text-white font-medium'>
+                        <a href="">
+                            <span>View all ~~&gt;</span>
+                        </a>
+                    </div>
+                </div>
+
+                {/* bottom */}
+            </div>
+        </>
     );
 };
 
